45cb6d02c47d8af2fe9587ed0e67815ad7c32242
jaeseopark <20038316+jaeseopark@users.noreply.github.com>
2022-04-26 15:03:48 -0600

    move code

--- crgw-app/crgw/handlers.py
+++ crgw-app/crgw/handlers.py
@@ -1,3 +1,4 @@
+import base64
 import logging
 import os
 from threading import Thread
@@ -5,7 +6,12 @@ from time import sleep
 from types import SimpleNamespace
 from typing import List
 
+import requests
+from pydash import url as pydash_url
+
 from crgw.config import get_config
+
+ALLOWED_FWD_HEADERS = ("rangeend", "rangestart", "content-type", "order", "nexttoken", "crg-method", "crg-body")
 
 LOGGER = logging.getLogger(__name__)
 
@@ -18,6 +24,24 @@ _LOCAL_FILE_CACHE = SimpleNamespace(
 
 def get_local_files() -> List[str]:
     return _LOCAL_FILE_CACHE.files
+
+
+def forward_request(data, headers: dict, method: str, subpath: str):
+    assert "CRG_REMOTE_HOST" in os.environ
+    assert "CRG_REMOTE_APIKEY" in os.environ
+
+    url = pydash_url(os.environ["CRG_REMOTE_HOST"], subpath)
+    headers = {k.lower(): v for k, v in headers.items() if k.lower() in ALLOWED_FWD_HEADERS}
+    headers["apikey"] = os.environ["CRG_REMOTE_APIKEY"]
+
+    if "crg-method" in headers:
+        assert "crg-body" in headers
+        method = headers.pop("crg-method")
+        headers["Content-Type"] = "application/json"
+        data = base64.b64decode(headers.pop("crg-body").encode()).decode().encode('utf-8')
+
+    r = requests.request(url=url, method=method, data=data, headers=headers)
+    return r.content, r.status_code, dict(r.headers)
 
 
 def teardown():
--- crgw-app/main.py
+++ crgw-app/main.py
@@ -1,13 +1,8 @@
-import base64
 import logging
-import os
-from urllib.parse import unquote_plus
 
-import requests
 from flask import Flask, request as freq, Response
-from pydash import url
 
-from crgw.handlers import get_local_files, teardown
+from crgw.handlers import get_local_files, teardown, forward_request
 
 logging.basicConfig(format="%(asctime)s %(levelname)s thread=%(thread)d %(module)s.%(funcName)s %(message)s")
 logging.root.setLevel(logging.INFO)
@@ -37,28 +32,11 @@ def fwd_remote(subpath: str):
       https://www.rfc-editor.org/rfc/rfc2616#section-10.3.3
     """
 
-    assert "CRG_REMOTE_HOST" in os.environ
-    assert "CRG_REMOTE_APIKEY" in os.environ
-    ALLOWED_HEADERS = ("rangeend", "rangestart", "content-type", "order", "nexttoken", "crg-method", "crg-body")
+    content, status_code, headers = forward_request(freq.data, dict(freq.headers), freq.method, subpath)
 
-    urll = url(os.environ["CRG_REMOTE_HOST"], subpath)
-
-    headers = {k.lower(): v for k, v in dict(freq.headers).items() if k.lower() in ALLOWED_HEADERS}
-    headers["apikey"] = os.environ["CRG_REMOTE_APIKEY"]
-
-    method = freq.method
-    data = freq.data
-    if "crg-method" in headers:
-        assert "crg-body" in headers
-        method = headers.pop("crg-method")
-        headers["Content-Type"] = "application/json"
-        data = base64.b64decode(headers.pop("crg-body").encode()).decode().encode('utf-8')
-
-    r = requests.request(url=urll, method=method, data=data, headers=headers)
-
-    res = Response(r.content)
-    res.headers = dict(r.headers)
-    res.status_code = r.status_code
+    res = Response(content)
+    res.headers = headers
+    res.status_code = status_code
     return res
 
 
