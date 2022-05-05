58fd73b0cbebcd03aebbfea21b7b80c714eccf0e
jaeseopark <20038316+jaeseopark@users.noreply.github.com>
2022-05-05 03:18:51 -0600

    cleaner

--- crgw-daemon/daemon/__main__.py
+++ crgw-daemon/daemon/__main__.py
@@ -7,7 +7,7 @@ from config.config import get_config
 from cleaner.cleaner import run_cleaner, init_cleaner_logger, init_cleaner_fs
 from watcher.watcher import run_watcher, init_watcher_logger, init_watcher_fs
 
-MODULES = (
+FUNCS = (
     (init_cleaner_fs, init_cleaner_logger, run_cleaner),
     (init_watcher_fs, init_watcher_logger, run_watcher)
 )
@@ -15,14 +15,15 @@ MODULES = (
 
 def run_daemon():
     config = get_config()
-    touch_directory(config["data"]["path"])
+    data_path = config["data"]["path"]
+    touch_directory(data_path)
 
     threads = []
 
-    for init_fs, init_logger, run_module in MODULES:
+    for init_fs, init_logger, run_module in FUNCS:
         init_fs(config)
         init_logger(config)
-        t = Thread(target=run_module, args=(config,))
+        t = Thread(target=run_cleaner, args=config)
         threads.append(t)
         t.start()
 
