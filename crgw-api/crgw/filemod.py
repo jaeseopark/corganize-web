eaae2b6472abcf0c8da739d1a9ab97a31c37d841
jaeseopark <20038316+jaeseopark@users.noreply.github.com>
2022-05-14 03:22:55 -0600

    Update filemod.py

--- crgw-api/crgw/filemod.py
+++ crgw-api/crgw/filemod.py
@@ -11,62 +11,68 @@ DEFAULT_EXT = ".mp4"
 
 
 def create_subclip(fileid: str, timerange: Tuple[int, int]) -> dict:
+    start, end = timerange
+    if start >= end:
+        raise ValueError("Negative duration")
+
+    def _callback(new_file: dict, source_path: str, target_path: str):
+        mimetype = new_file.get("mimetype")
+        ext_with_dot = mimetypes.guess_extension(mimetype) if mimetype else DEFAULT_EXT
+        tmp_path = target_path + ext_with_dot
+        ffmpeg_extract_subclip(source_path, start, end, targetname=tmp_path)
+        os.rename(tmp_path, target_path)
+
+    def _decorate_new_file(new_file: dict) -> dict:
+        multimedia = new_file.get("multimedia", dict())
+        multimedia["duration"] = end - start
+        new_file["filename"] = f"{new_file['filename']}-{start}-{end}"
+        new_file["multimedia"] = multimedia
+        return new_file
+
+    new_fileid = f"{fileid}-{start}-{end}"
+    return _process(fileid, new_fileid, decorate_new_file=_decorate_new_file, callback=_callback)
+
+
+def trim_clip(fileid: str, segments: List[dict]) -> dict:
+    if len(segments) == 0:
+        raise ValueError("Provide more than zero segments")
+
+
+def _process(fileid: str,
+             new_fileid: str,
+             decorate_new_file: Callable[[dict], dict],
+             callback: Callable[[dict, str, str], None]) -> dict:
+    crg_client = CorganizeClient(os.environ["CRG_REMOTE_HOST"], os.environ["CRG_REMOTE_APIKEY"])
+
     source_path = os.path.join(DATA_PATH, fileid + ".dec")
     if not os.path.exists(source_path):
         raise FileNotFoundError
 
-    crg_client = CorganizeClient(os.environ["CRG_REMOTE_HOST"], os.environ["CRG_REMOTE_APIKEY"])
-
-    starttime, endtime = timerange
-    if starttime >= endtime:
-        raise ValueError("Negative duration")
-    new_fileid = f"{fileid}-{starttime}-{endtime}"
-    target_path = os.path.join(DATA_PATH, new_fileid + ".dec")
-
-    def _execute(mimetype: str):
-        ext_with_dot = mimetypes.guess_extension(mimetype) if mimetype else DEFAULT_EXT
-        tmp_path = target_path + ext_with_dot
-        ffmpeg_extract_subclip(source_path, starttime, endtime, targetname=tmp_path)
-        os.rename(tmp_path, target_path)
-
-    new_file = _create_subclip(fileid, new_fileid, starttime, endtime, crg_client, _execute)
-    new_file.update(dict(
-        size=os.stat(target_path).st_size,
-        lastopened=0,
-    ))
-
-    crg_client.update_file(new_file)
-
-    return new_file
-
-
-def _create_subclip(fileid, new_fileid, starttime, endtime, crg_client, execute: Callable[[str], None]):
     source_file = crg_client.get_file(fileid)
 
     mimetype = source_file.get("mimetype")
-    multimedia = merge(
-        source_file.get("multimedia") or dict(),
-        dict(duration=endtime - starttime)
-    )
+    multimedia = source_file.get("multimedia") or dict()
     if "highlights" in multimedia:
         multimedia.pop("highlights")
 
-    new_file = dict(
+    new_file = decorate_new_file(dict(
         fileid=new_fileid,
-        filename=f"{source_file['filename']}-{starttime}-{endtime}",
+        filename=source_file["filename"],
         sourceurl=source_file["sourceurl"],
         storageservice="local",
         locationref="local",
         mimetype=mimetype,
         multimedia=multimedia,
         dateactivated=now_seconds()
-    )
+    ))
     crg_client.create_files([new_file])
 
-    execute(mimetype)
+    target_path = os.path.join(DATA_PATH, new_fileid + ".dec")
+    callback(new_file, source_path, target_path)
+    new_file.update(dict(
+        size=os.stat(target_path).st_size,
+        lastopened=0,
+    ))
 
+    crg_client.update_file(new_file)
     return new_file
-
-
-def trim_clip(fileid: str, segments: List[dict]) -> dict:
-    pass
