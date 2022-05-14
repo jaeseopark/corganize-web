6a0bcdd2dce2b4bded4aafd3689d04b86c6dcec0
jaeseopark <20038316+jaeseopark@users.noreply.github.com>
2022-05-14 12:03:33 -0600

    Update postprocessing.py

--- crgw-api/crgw/postprocessing.py
+++ crgw-api/crgw/postprocessing.py
@@ -41,7 +41,7 @@ def cut_clip(fileid: str, segments: List[Tuple[int, int]]) -> List[dict]:
             ffmpeg_extract_subclip(source_path, start, end, targetname=tmp_path)
             os.rename(tmp_path, target_path)
 
-        new_file = _process(fileid, suffix=f"-{start}-{end}", new_duration=end - start, run_postprocessing=ffmpeg_subclip)
+        new_file = _process(fileid, suffix=f"-{start}-{end}", new_duration=end - start, process=ffmpeg_subclip)
         new_files.append(new_file)
 
     return new_files
@@ -51,46 +51,43 @@ def trim_clip(fileid: str, segments: List[Tuple[int, int]]) -> dict:
     segments = normalize_segments(segments)
     duration = sum([end - start for start, end in segments])
 
-    def get_filter_describer() -> str:
-        # Credit: https://gist.github.com/FarisHijazi/eff7a7979440faa84a63657e085ec504
-        filter_describer = ""
-        for i, (start, end) in enumerate(segments):
-            filter_describer += f"[0:v]trim=start={start}:end={end},setpts=PTS-STARTPTS[{i}v]; "  # format=yuv420p ?
-            filter_describer += f"[0:a]atrim=start={start}:end={end},asetpts=PTS-STARTPTS[{i}a]; "
+    def get_command(source_path: str, target_path: str) -> List[str]:
+        # Credit: https://superuser.com/a/1621363
+        cmd: List[str] = [get_moviepy_setting("FFMPEG_BINARY"), "-y", ]
+        for start, end in segments:
+            cmd.append(f"-ss {start} -to {end} -i {source_path}")
 
-        for i in range(len(segments)):
-            filter_describer += f"[{i}v][{i}a]"
+        tracks = "".join([f"[{i}:v][{i}:a]" for i in range(len(segments))])
+        filter_describer = f"{tracks}concat=n={len(segments)}:v=1:a=1[outv][outa]"
 
-        filter_describer += f"concat=n={len(segments)}:v=1:a=1[outv][outa]"
-        return filter_describer
+        cmd += [
+            "-filter_complex", filter_describer,
+            "-map", "[outv]",
+            "-map", "[outa]",
+            target_path
+        ]
+
+        return cmd
 
     def ffmpeg_filter_trim(new_file: dict, source_path: str, target_path: str):
         mimetype = new_file.get("mimetype")
         ext_with_dot = mimetypes.guess_extension(mimetype) if mimetype else DEFAULT_EXT
         tmp_path = target_path + ext_with_dot
 
-        cmd: List[str] = [
-            get_moviepy_setting("FFMPEG_BINARY"),
-            "-i", source_path,
-            "-y",
-            "-filter_complex", get_filter_describer(),
-            "-map", "[outv]",
-            "-map", "[outa]",
-            tmp_path
-        ]
+        cmd = get_command(source_path, target_path=tmp_path)
         LOGGER.info(f"{' '.join(cmd)=}")
 
         subprocess_call(cmd)
         os.rename(tmp_path, target_path)
 
     suffix = f"-trim-{md5(str(segments))[:TRIMID_LENGTH]}"
-    return _process(fileid, suffix=suffix, new_duration=duration, run_postprocessing=ffmpeg_filter_trim)
+    return _process(fileid, suffix=suffix, new_duration=duration, process=ffmpeg_filter_trim)
 
 
 def _process(fileid: str,
              suffix: str,
              new_duration: int,
-             run_postprocessing: Callable[[dict, str, str], None]) -> dict:
+             process: Callable[[dict, str, str], None]) -> dict:
     source_path = os.path.join(DATA_PATH, fileid + ".dec")
     if not os.path.exists(source_path):
         message = f"file not found: {source_path=}"
@@ -119,7 +116,7 @@ def _process(fileid: str,
         lastopened=0,
     )
 
-    run_postprocessing(new_file, source_path, target_path)
+    process(new_file, source_path, target_path)
 
     new_file.update(dict(
         size=os.stat(target_path).st_size,
