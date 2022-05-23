ed7eda566fa4b7bf10dd194489784e6b0ac65b17
Jaeseo Park <jaeseopark@icloud.com>
2022-05-23 11:54:12 -0600

    fix: style linter

--- crgw-ui/src/components/standalone/fileview/video/SegmentBlock.jsx
+++ crgw-ui/src/components/standalone/fileview/video/SegmentBlock.jsx
@@ -1,18 +1,44 @@
 import cls from "classnames";
 import styled from "styled-components";
 
-const SegmentBlock = ({ segment, duration, isOpen = false, isLeadIndicator = false, isHighlight = false }) => {
+const SegmentBlock = ({
+  segment,
+  duration,
+  isOpen = false,
+  isLeadIndicator = false,
+  isHighlight = false,
+}) => {
   const left = (segment.start / duration) * 100;
   const right = (1 - segment.end / duration) * 100;
   const clsName = cls("segment", { open: isOpen, lead: isLeadIndicator, highlight: isHighlight });
   return <StyledBlock className={clsName} left={`${left}%`} right={`${right}%`} />;
 };
 
-export const ClosedSegmentBlock = ({ segment, duration }) => <SegmentBlock segment={segment} duration={duration} />;
-export const OpenSegmentBlock = ({ duration, start, currentTime }) => <SegmentBlock duration={duration} segment={{ start, end: currentTime }} isOpen={true} />;
-export const Seeker = ({ currentTime, duration }) => <SegmentBlock segment={{ start: currentTime, end: currentTime + 1 }} duration={duration} isLeadIndicator={true} />;
-export const Highlight = ({ timestamp, duration }) => <SegmentBlock segment={{ start: timestamp, end: timestamp + 1 }} duration={duration} isHighlight={true} />;
-export const TimeMarker = ({ value }) => <StyledTimeMarker className="time-marker" left={value * 10}>{value}</StyledTimeMarker>;
+export const ClosedSegmentBlock = ({ segment, duration }) => (
+  <SegmentBlock segment={segment} duration={duration} />
+);
+export const OpenSegmentBlock = ({ duration, start, currentTime }) => (
+  <SegmentBlock duration={duration} segment={{ start, end: currentTime }} isOpen={true} />
+);
+export const Seeker = ({ currentTime, duration }) => (
+  <SegmentBlock
+    segment={{ start: currentTime, end: currentTime + 1 }}
+    duration={duration}
+    isLeadIndicator={true}
+  />
+);
+export const Highlight = ({ timestamp, duration }) => (
+  <SegmentBlock
+    segment={{ start: timestamp, end: timestamp + 1 }}
+    duration={duration}
+    isHighlight={true}
+  />
+);
+export const TimeMarker = ({ value }) => (
+  <StyledTimeMarker className="time-marker" left={value * 10}>
+    {value}
+  </StyledTimeMarker>
+);
 
 const StyledBlock = styled.div`
   left: ${({ left }) => left};
