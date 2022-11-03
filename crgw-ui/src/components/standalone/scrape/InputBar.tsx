e7e77e35274c84c42dc94a4ee1e5d8d0c44b053f
Jaeseo <jaeseopark@git.com>
2022-11-03 15:20:59 -0600

    fix: prevent unnecessary rerender

--- crgw-ui/src/components/standalone/scrape/InputBar.tsx
+++ crgw-ui/src/components/standalone/scrape/InputBar.tsx
@@ -18,7 +18,7 @@ import { sample } from "utils/arrayUtils";
 
 import { CARD_STATUS, Card } from "components/standalone/scrape/ScrapePanelCardView";
 
-const BULK_ADD_OPTIONS = [10, 25, 50, 100];
+const BULK_ADD_OPTIONS = [10, 25, 50, 9999];
 
 type ScrapeInputBarProps = {
   disabled: boolean;
@@ -30,6 +30,71 @@ type ScrapeInputBarProps = {
   rawScrapeCount: number;
   localScrapeHost: boolean;
   setLocalScrapeHost: (b: boolean) => void;
+};
+
+const AddButtonGroup = ({
+  createFilesFromCards,
+  filterCards,
+  disabled,
+}: {
+  createFilesFromCards: (cards: Card[]) => void;
+  filterCards: (status: string) => Card[];
+  disabled: boolean;
+}) => {
+  const selectRef = useRef<HTMLSelectElement | null>(null);
+
+  const addCards = (cards: Card[], sampleSize?: number) => {
+    if (sampleSize) {
+      cards = sample(cards, sampleSize);
+    }
+    createFilesFromCards(cards);
+  };
+
+  const getAvailableCards = () => filterCards(CARD_STATUS.AVAILABLE);
+
+  const getOptions = () => {
+    const fromStartOptions = BULK_ADD_OPTIONS.map((cnt) => ({
+      label: `First ${cnt}`,
+      onClick: () => addCards(getAvailableCards().slice(0, cnt)),
+    }));
+
+    const randomOptions = BULK_ADD_OPTIONS.map((cnt) => ({
+      label: `${cnt} random`,
+      onClick: () => addCards(getAvailableCards(), cnt),
+    }));
+
+    const fromEndOptions = BULK_ADD_OPTIONS.map((cnt) => ({
+      label: `Last ${cnt}`,
+      onClick: () => addCards(getAvailableCards().slice(-cnt)),
+    }));
+
+    return [...fromStartOptions, ...randomOptions, ...fromEndOptions];
+  };
+
+  const isZeroCards = filterCards(CARD_STATUS.AVAILABLE).length === 0;
+  const options = getOptions();
+
+  return (
+    <ButtonGroup className="add-group">
+      <Select variant="filled" disabled={disabled || isZeroCards} ref={selectRef}>
+        {options.map(({ label }) => (
+          <option key={label}>{label}</option>
+        ))}
+      </Select>
+      <Button
+        className="add-button"
+        onClick={() => {
+          if (selectRef?.current) {
+            const { onClick } = options[selectRef?.current.selectedIndex];
+            onClick();
+          }
+        }}
+        disabled={disabled || isZeroCards}
+      >
+        Add
+      </Button>
+    </ButtonGroup>
+  );
 };
 
 const ScrapeInputBar = ({
@@ -45,63 +110,6 @@ const ScrapeInputBar = ({
 }: ScrapeInputBarProps) => {
   const { protectHotkey, exposeHotkey } = useBlanket();
   const filterCards = (status: string) => cards.filter((c) => c.status === status);
-
-  const getAvailableCards = () => filterCards(CARD_STATUS.AVAILABLE);
-
-  const addCards = (cards: Card[], sampleSize?: number) => {
-    if (sampleSize) {
-      cards = sample(cards, sampleSize);
-    }
-    createFilesFromCards(cards);
-  };
-
-  const AddButtonGroup = () => {
-    const selectRef = useRef<HTMLSelectElement | null>(null);
-
-    const getOptions = () => {
-      const fromStartOptions = BULK_ADD_OPTIONS.map((cnt) => ({
-        label: `First ${cnt}`,
-        onClick: () => addCards(getAvailableCards().slice(0, cnt)),
-      }));
-
-      const randomOptions = BULK_ADD_OPTIONS.map((cnt) => ({
-        label: `${cnt} random`,
-        onClick: () => addCards(getAvailableCards(), cnt),
-      }));
-
-      const fromEndOptions = BULK_ADD_OPTIONS.map((cnt) => ({
-        label: `Last ${cnt}`,
-        onClick: () => addCards(getAvailableCards().slice(-cnt)),
-      }));
-
-      return [...fromStartOptions, ...randomOptions, ...fromEndOptions];
-    };
-
-    const isZeroCards = filterCards(CARD_STATUS.AVAILABLE).length === 0;
-    const options = getOptions();
-
-    return (
-      <ButtonGroup className="add-group">
-        <Select variant="filled" disabled={disabled || isZeroCards} ref={selectRef}>
-          {options.map(({ label }) => (
-            <option key={label}>{label}</option>
-          ))}
-        </Select>
-        <Button
-          className="add-button"
-          onClick={() => {
-            if (selectRef?.current) {
-              const { onClick } = options[selectRef?.current.selectedIndex];
-              onClick();
-            }
-          }}
-          disabled={disabled || isZeroCards}
-        >
-          Add
-        </Button>
-      </ButtonGroup>
-    );
-  };
 
   const countByStatus = Array.from(new Set(cards.map((c) => c.status)))
     .sort()
@@ -139,7 +147,11 @@ const ScrapeInputBar = ({
             <Button className="scrape-button" type="submit" disabled={disabled || !url}>
               Scrape
             </Button>
-            <AddButtonGroup />
+            <AddButtonGroup
+              filterCards={filterCards}
+              createFilesFromCards={createFilesFromCards}
+              disabled={disabled}
+            />
           </Flex>
           <HStack className="form-row metadata">
             <Spacer flexGrow={1} />
