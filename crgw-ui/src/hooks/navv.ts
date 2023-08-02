966dde5103f686924eee0387ab1d8894e669ab7d
jaeseopark <20038316+jaeseopark@users.noreply.github.com>
2023-08-02 08:47:53 -0600

    feat: blank scrape

--- crgw-ui/src/hooks/navv.ts
+++ crgw-ui/src/hooks/navv.ts
@@ -8,6 +8,7 @@ export const useNavv = () => {
   return {
     navRoot: () => navigate("/"),
     navToAdmin: () => navigate("/admin"),
+    navBlankScrape: () => navigate('/scrape'),
     navJson: (file: CorganizeFile) => navigate(`/file/${file.fileid}/json`),
     navContent: (file: CorganizeFile) => navigate(`/file/${file.fileid}/content`),
     navTags: (file: CorganizeFile) => navigate(`/file/${file.fileid}/tags`),
