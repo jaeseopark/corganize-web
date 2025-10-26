3db4d360de509f16cb45570997b126208c4dc191
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2025-10-26 01:49:22 -0600

    refactor: move logger init to main daemon module

--- crgw-daemon/cleaner/__init__.py
+++ crgw-daemon/cleaner/__init__.py
@@ -1,9 +1,5 @@
-import logging
 import os
-
-from commmons import init_logger_with_handlers
 
 
 def init_cleaner(config: dict):
     os.makedirs(config["data"]["path"], exist_ok=True)
-    init_logger_with_handlers("cleaner", logging.DEBUG, config["log"]["cleaner"])
